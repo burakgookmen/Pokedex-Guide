7694f16482770fc03955a36d40b11002a4d4436f
Burak <146767454+burakgookmen@users.noreply.github.com>
2024-08-07 21:38:37 +0300

    Update README.m

--- README.m
+++ README.m
@@ -1,1 +1,40 @@
+# Pokedex Guide
 
+## Genel Bakış
+
+Bu proje, Javascript kullanarak Arama yapma işleminin nasıl yapıldığını gösterir.
+
+Pokedex Guide, tüm Pokémon hayranlarının yanlarında taşımak isteyeceği kapsamlı bir Pokémon rehberidir. Bu uygulama, Pokémon evrenindeki tüm yaratıklar hakkında ayrıntılı bilgi sağlar. 
+
+## Özellikler
+
+- Her bir Pokémon'un ayrıntılı bilgileri: tür, yetenekler, istatistikler ve daha fazlası.
+- Arama işleminin kolaylaştırılması
+- Kolay arama fonksiyonu ile istediğiniz Pokémon'u hızlıca bulun.
+- Tür, bölge, yetenek ve daha fazlasına göre filtreleme.
+
+## Kurulum
+
+1. Bu depoyu indirin:
+    ```bash
+    git clone https://github.com/burakgookmen/Pokedex-Guide.git
+    ```
+
+2. Vscode Aracılığıyla Proje Klasörünü Açın
+
+3. Html dosyasını başlatın
+
+## Kullanım
+
+1. Uygulama başlatıldığında, arama çubuğuna Pokemonun adını girin ve "Ara" butonuna tıklayın.
+2. Aranan Pokemonun türü, yetenekleri ve istatistikleri görüntülenecektir.
+
+## Kullanılan Teknolojiler
+
+**İstemci:** HTML, CSS, Javascript
+
+**Editör:** Visual Studio Code
+
+## İletişim
+
+Bu projeyle ilgili sorularınız veya önerileriniz için, lütfen [buraakgookmen@gmail.com](mailto:buraakgookmen@gmail.com) adresine e-posta gönderin.
